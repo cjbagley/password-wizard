cf8c8347f7b9eb432fe2a8e010459f338e40e95c
Colin Bagley <cjbagley@googlemail.com>
2024-02-16 13:47:27 +0000

    feature: Sha1 hash of given string

--- password_wizard/api/hibp.py
+++ password_wizard/api/hibp.py
@@ -1,6 +1,8 @@
+import hashlib
+
 class HIBP():
     """ Have I Been Pwned functionality for calling the API """
 
-    def test(self) -> bool:
-        return True
+    def hash(self, s: str) -> str:
+        return hashlib.sha1(s.encode()).hexdigest()
 
--- tests/api/test_hibp.py
+++ tests/api/test_hibp.py
@@ -3,7 +3,12 @@ from unittest import TestCase
 from password_wizard.api.hibp import HIBP
 
 class TestHIBP(TestCase):
-    def test_a(self):
+    def test_hibp_hash_password(self):
         hibp = HIBP()
-        result = hibp.test()
-        self.assertTrue(result)
+        expected = "5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8"
+        result = hibp.hash("password")
+        self.assertEqual(result, expected)
+
+        expected = "a94a8fe5ccb19ba61c4c0873d391e987982fbbd3"
+        result = hibp.hash("test")
+        self.assertEqual(result, expected)
