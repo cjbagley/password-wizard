d91c1d33d1626449081864caea185985fa602d40
Colin Bagley <cjbagley@googlemail.com>
2024-02-16 14:11:07 +0000

    feature: basic call to pwnpasswords range api

--- password_wizard/api/hibp.py
+++ password_wizard/api/hibp.py
@@ -1,8 +1,17 @@
 import hashlib
+import requests
 
 class HIBP():
     """ Have I Been Pwned functionality for calling the API """
+    def __init__(self) -> None:
+        self._api_endpoint = "https://api.pwnedpasswords.com/range/"
 
     def hash(self, s: str) -> str:
         return hashlib.sha1(s.encode()).hexdigest()
 
+    def getResults(self, password: str) -> list[str]:
+        hashed = self.hash(password)
+        chunk = hashed[:5]
+        headers = {"Add-Padding": "true"}
+        r = requests.get(self._api_endpoint + chunk, headers=headers)
+        return r.text.splitlines()
--- tests/api/test_hibp.py
+++ tests/api/test_hibp.py
@@ -3,7 +3,7 @@ from unittest import TestCase
 from password_wizard.api.hibp import HIBP
 
 class TestHIBP(TestCase):
-    def test_hibp_hash_password(self):
+    def test_hash(self):
         hibp = HIBP()
         expected = "5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8"
         result = hibp.hash("password")
@@ -12,3 +12,8 @@ class TestHIBP(TestCase):
         expected = "a94a8fe5ccb19ba61c4c0873d391e987982fbbd3"
         result = hibp.hash("test")
         self.assertEqual(result, expected)
+    
+    def test_get_results(self):
+        hibp = HIBP()
+        results = hibp.getResults("password")
+        self.assertTrue(len(results) > 700)
