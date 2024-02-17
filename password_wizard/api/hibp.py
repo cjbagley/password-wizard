b45c130e996ca07f5f94fe434e3f1508658b47cd
Colin Bagley <cjbagley@googlemail.com>
2024-02-17 13:05:18 +0000

    feature: Functionality to check a search hash

--- password_wizard/api/hibp.py
+++ password_wizard/api/hibp.py
@@ -1,12 +1,19 @@
 """ Have I Been Pwned functionality for calling the API """
+
 import requests
-from password_wizard.utils.utils import sha1_hash
+import re
 
 HIBP_ENDPOINT = "https://api.pwnedpasswords.com/range/"
 
-def getResults(password: str) -> list[str]:
-    hashed = sha1_hash(password)
-    chunk = hashed[:5]
+def get_matching_hashes(search_hash: str) -> list[str]:
+    search_hash = search_hash.upper()
+    _check_search_hash(search_hash)
     headers = {"Add-Padding": "true"}
-    r = requests.get(HIBP_ENDPOINT + chunk, headers=headers)
+    r = requests.get(HIBP_ENDPOINT + search_hash, headers=headers)
     return r.text.splitlines()
+
+def _check_search_hash(search_hash: str) -> None:
+    if (len(search_hash) != 5):
+        raise ValueError("Search hash must be the first 5 characters only")
+    if (re.match(r"^[A-F0-9]{5}$", search_hash) == None):
+        raise ValueError("Search hash given looks to be plain text")
--- tests/api/test_hibp.py
+++ tests/api/test_hibp.py
@@ -1,7 +1,10 @@
 from unittest import TestCase
-from password_wizard.api.hibp import getResults
+from password_wizard.api.hibp import get_matching_hashes
 
 class TestHIBP(TestCase):
-    def test_get_results(self):
-        results = getResults("password")
-        self.assertTrue(len(results) > 700)
+    def test_get_matching_hashes(self):
+        with self.assertRaises(ValueError):
+            get_matching_hashes("password")
+        with self.assertRaises(ValueError):
+            get_matching_hashes("pass")
+        self.assertTrue(len(get_matching_hashes("5baa6")) > 700)
