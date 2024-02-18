d91888bd3963ea9a5b4b9dd6704e5dba6634ddeb
Colin Bagley <cjbagley@googlemail.com>
2024-02-18 09:58:56 +0000

    feature: docs for HIBP API

--- password_wizard/api/hibp.py
+++ password_wizard/api/hibp.py
@@ -1,4 +1,17 @@
-""" Have I Been Pwned functionality for calling the API """
+""" Functions related to interacting with 'Have I Been Pwned' (HIBP) API
+
+    Full details of how the API works can be found at:
+    https://haveibeenpwned.com/API/v3#SearchingPwnedPasswordsByRange
+    In short summary: it uses k-Anonymity model that allows searching
+    previously leaked passwords by the first 5 characters of a SHA-1 hash.
+    The password itself is never sent via the API; just the first 5
+    characters of the hash. The API returns any matching hashes that
+    start with those 5 characters (but only the remaining parts of the 
+    full hash string, i.e. they do not have the first 5 characters).
+    The results can then be combined with the original 5 characters locally
+    to find if any combined hash matches the full hash of the password.
+    If a match has been found: that password has been leaked at some point.
+"""
 
 import requests
 import re
@@ -7,6 +20,12 @@ HIBP_ENDPOINT = "https://api.pwnedpasswords.com/range/"
 
 
 def get_matching_hashes(search_hash: str) -> list[str]:
+    """Call HIBP API to find any matching hashes
+
+    Results from the API come as a text string:
+    {matching hash remainder}:{count of times leaked}\n
+    {matching hash remainder}:{count of times leaked}\n...
+    """
     search_hash = search_hash.upper()
     _check_search_hash(search_hash)
     headers = {"Add-Padding": "true"}
@@ -15,7 +34,10 @@ def get_matching_hashes(search_hash: str) -> list[str]:
 
 
 def _check_search_hash(search_hash: str) -> None:
+    """Helper function to check if string is in expected search hash format"""
     if len(search_hash) != 5:
         raise ValueError("Search hash must be the first 5 characters only")
     if re.match(r"^[A-F0-9]{5}$", search_hash) is None:
-        raise ValueError("Search hash given looks to be plain text")
+        raise ValueError(
+            "Search hash given looks to be plain text - please provide a hash"
+        )
