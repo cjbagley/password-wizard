53e1bfe926a08174c025faef0b21d2ffd998c738
Colin Bagley <cjbagley@googlemail.com>
2024-02-20 19:41:54 +0000

    feature: basic password string generation

--- password_wizard/utils/password_generator.py
+++ password_wizard/utils/password_generator.py
@@ -1,5 +1,8 @@
 """ Password Generator - Used to generate a password based on selected options """
 
+import random
+import string
+
 
 class PasswordGenerator:
     """Used to generate a password based on the class attributes:
@@ -11,14 +14,24 @@ class PasswordGenerator:
     def __init__(self) -> None:
         """Default the options for the generated password"""
         self._length = 18
+        self._character_list = string.ascii_letters + string.digits
+        self._character_list += string.punctuation
 
     def set_length(self, length: int) -> None:
         """Set the length of the generated password"""
         self._length = length
+
+    def set_seed(self, seed: int = 0) -> None:
+        """Set the random seed, to be used when testing"""
+        random.seed(seed)
 
     def generate(self) -> str:
         """Return a randomly generated password
         Uses the class options to determine what the password
         should contain.
         """
-        return "password"
+        password = ""
+        for _ in range(self._length):
+            password += random.choice(self._character_list)
+
+        return password
