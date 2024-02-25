ae450f66597c4cacb9861a7f719c23ab9f9697a6
Colin Bagley <cjbagley@googlemail.com>
2024-02-25 13:18:47 +0000

    feature: Add CLI check option test

--- tests/cli/test_cli.py
+++ tests/cli/test_cli.py
@@ -1,9 +1,12 @@
+import re
 from unittest import TestCase
+from unittest.mock import patch
+from password_wizard.cli.options.check import Check
 from password_wizard.cli.options.generate_string import GenerateString
 from password_wizard.cli.cli import get_option_parser
 
 
-class TestGenerateString(TestCase):
+class TestCLI(TestCase):
     def test_generate_string(self) -> None:
         option = GenerateString()
         parser = get_option_parser([option])
@@ -19,3 +22,16 @@ class TestGenerateString(TestCase):
 
         with self.assertRaises(ValueError):
             parser.parse_args([option.get_command_name(), "-not-existing", "10"])
+
+    @patch("password_wizard.cli.options.check.getpass", return_value="password")
+    def test_check(self, mock) -> None:
+        """Note: mock must be passed in as func arg for mocking purposes, even if not used below"""
+        option = Check()
+        parser = get_option_parser([option])
+        args = parser.parse_args([option.get_command_name()])
+        output = option.execute(args)
+        self.assertTrue(output.exit_code == 0)
+
+        self.assertIsNotNone(
+            re.search("^This password has been found \\d+ times", output.output)
+        )
